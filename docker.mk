884753233f0b7dfbb5f5071be9b408c4b910582f
Christian Birk Sørensen <cbs@silicom.dk>
2022-10-29 19:19:48 +0200

    Removed cleanall.mk

--- docker.mk
+++ docker.mk
@@ -28,4 +28,17 @@ COCOTB_HDL_TIMEUNIT = 1ps
 COCOTB_HDL_TIMEPRECISION = 1ps
 
 include $(shell cocotb-config --makefiles)/Makefile.sim
-include cleanall.mk
+
+cleanall: clean
+	@rm -rf output
+	@rm -rf __pycache__
+	@rm -rf results.xml
+	@rm -rf log.txt
+	@rm -rf sim_build
+	@rm -rf modelsim.ini
+	@rm -rf transcript
+	@rm -rf waveform.vcd
+	@rm -rf DVEfiles
+	@rm -rf *.vcd
+	@rm -rf *.vpd
+	@rm -rf ucli.key
